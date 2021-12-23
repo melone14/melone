a4c12baaafc9d412ed2f758f14dcc9c09ca0d6c5
melone14 <w.melone97@gmail.com>
2021-12-23 11:21:24 +0100

    Add global styles and theme with styled-components

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#383838',
+    success: '#8FCB81',
+    error: '#CB8581',
+    warning: '#E1D888',
+  },
+  fontSize: {
+    l: '17px',
+    m: '12px',
+    s: '11px',
+  },
+};
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,3 +1,14 @@
-const Root = () => <div>siema! sss</div>;
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
+
+const Root = () => (
+  <div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <h1>Siemanko</h1>
+    </ThemeProvider>
+  </div>
+);
 
 export default Root;
