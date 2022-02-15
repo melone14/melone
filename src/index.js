b6117b938cc528a2972482ddbd49e9c162b68294
melone14 <w.melone97@gmail.com>
2022-02-15 10:48:31 +0100

    back on track commit xd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from 'templates/ScrollToTop/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Root />
+      <ScrollToTop>
+        <Root />
+      </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
